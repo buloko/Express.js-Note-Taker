642a27e3e0a0dec1db580a0c06d65684239b83b5
buloko <buloko8@gmail.com>
2023-04-01 12:02:58 -0400

    setting up the get routes and middleware

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+
+
+
+
+const app = express();
+
+//Middleware for parsing JSON and urlencoded from data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", api);
+
+app.use(express.static('public'));
+
+//Get route for homepage
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+//get route for note taking page
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
